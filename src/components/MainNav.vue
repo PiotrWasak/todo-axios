eeb55a3f4c36fb864ec8bb3214d384e8763d5552
PiotrWasak <piotrwasakgdynia@gmail.com>
2021-11-10 08:33:52 +0100

    com

---
+++ src/components/MainNav.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <v-system-bar color="deep-purple darken-3"></v-system-bar>
+
+    <v-app-bar color="deep-purple accent-4" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>TODO</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-filter</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" absolute bottom temporary>
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <router-link to="/home"
+              ><v-list-item-title>Tasks</v-list-item-title></router-link
+            >
+          </v-list-item>
+
+          <v-list-item>
+            <router-link to="/profile"
+              ><v-list-item-title>Profile</v-list-item-title></router-link
+            >
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+<script>
+export default {
+  name: "MainNav",
+  data() {
+    return {
+      drawer: false,
+    };
+  },
+};
+</script>
+<style lang=""></style>
