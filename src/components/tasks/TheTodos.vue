7e6d0e7328424cecb5de4148ec0bd5a09c051890
PiotrWasak <piotrwasakgdynia@gmail.com>
2021-11-18 14:26:37 +0100

    dbutils

--- src/components/tasks/TheTodos.vue
+++ src/components/tasks/TheTodos.vue
@@ -1,10 +1,15 @@
 <template>
   <v-container>
-    <todo-add
-      v-on:add-task="postToDatabase"
-    ></todo-add>
-    <todo-list v-on:deleteTask="deleteFromDatabase" v-on:updateStatus="updateDatabase" :task-list="taskList"></todo-list>
-    <todo-list-done  v-on:updateStatus="updateDatabase" :done-task-list="doneTaskList"></todo-list-done>
+    <todo-add v-on:add-task="postToDatabase"></todo-add>
+    <todo-list
+      v-on:deleteTask="deleteFromDatabase"
+      v-on:updateStatus="updateDatabase"
+      :task-list="taskList"
+    ></todo-list>
+    <todo-list-done
+      v-on:updateStatus="updateDatabase"
+      :done-task-list="doneTaskList"
+    ></todo-list-done>
   </v-container>
 </template>
 
@@ -12,9 +17,15 @@
 import TodoAdd from "@/components/tasks/TodoAdd";
 import TodoList from "@/components/tasks/TodoList";
 import TodoListDone from "@/components/tasks/TodoListDone";
-import axios from "axios";
 import { userDataMixin } from "@/mixins/userDataMixin";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import {
+  getUserData,
+  postToDatabase,
+  updateDatabase,
+  getFromDatabase,
+  deleteFromDatabase,
+} from "@/utils/databaseFunctions";
+
 export default {
   name: "TheTodoList",
   components: { TodoListDone, TodoList, TodoAdd },
@@ -31,80 +42,11 @@ export default {
     },
   },
   methods: {
-    getUserData() {
-      const auth = getAuth();
-      onAuthStateChanged(auth, (user) => {
-        if (user) {
-          this.userData = user;
-          this.getFromDatabase();
-        } else {
-          console.log("User not logged in");
-        }
-      });
-    },
-    postToDatabase(taskValue) {
-      const taskData = {
-        task: taskValue,
-        status: "todo",
-      };
-      axios
-        .post(`/${this.userData.uid}.json`, taskData)
-        .then(() => {
-          this.getFromDatabase();
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-    updateDatabase(taskId, status) {
-      axios
-        .patch(`${this.userData.uid}/${taskId}.json`, {
-          status: status,
-        })
-        .then(() => {
-          this.getFromDatabase();
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-    getFromDatabase() {
-      this.taskList.splice(0, this.taskList.length);
-      this.doneTaskList.splice(0, this.doneTaskList.length);
-      axios
-        .get(`${this.userData.uid}.json`)
-        .then((response) => {
-          Object.entries(response.data).forEach((element) => {
-            const task = {
-              uid: element[0],
-              value: element[1].task,
-              status: element[1].status,
-            };
-            if (task.status === "todo") {
-              this.taskList.push(task);
-            } else if (task.status === "done") {
-              this.doneTaskList.push(task);
-            }
-          });
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-    deleteFromDatabase(taskId) {
-      axios
-        .delete(`${this.userData.uid}/${taskId}.json`)
-        .then(() => {
-          console.log("Succesfully deleted");
-          this.getFromDatabase();
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-  },
-  created() {
-    //this.getUserData();
+    getUserData,
+    postToDatabase,
+    updateDatabase,
+    getFromDatabase,
+    deleteFromDatabase,
   },
 };
 </script>
